0ff1d85ba6bd873e945cb5a954a916d79cdc9de4
Justin York <justincyork@gmail.com>
2013-01-14 16:59:35 -0700

    Simplify and enhance interface

--- example.js
+++ example.js
@@ -1,21 +1,21 @@
 /*
 
 # jQuery.rest API in 10 seconds or less:
-$.rest.get,  $.rest.read,   $.rest.prototype.get,  $.rest.prototype.read
-$.rest.post, $.rest.create, $.rest.prototype.post, $.rest.prototype.create
-$.rest.put,  $.rest.update, $.rest.prototype.put,  $.rest.prototype.update
-$.rest.del,  $.rest.delete, $.rest.prototype.del,  $.rest.prototype.delete
+$.rest.get,  $.rest.prototype.get
+$.rest.post, $.rest.prototype.post
+$.rest.put,  $.rest.prototype.put
+$.rest.del,  $.rest.prototype.del
 
 */
 
-var twttr = new $.rest("http://api.twitter.com/1", ".json", true);
+var twttr = new $.rest("http://api.twitter.com/1");
 
-twttr.get("/statuses/show/20", function (data) {  });
-	// GET http://api.twitter.com/1/statuses/show/20.json
+// GET http://api.twitter.com/1/statuses/show/20.json
+twttr.get("/statuses/show/20.json", function (data) {  });
 
-twttr.del("/statuses/destroy/20", "destroy", 20]);
-	// DELETE http://api.twitter.com/1/statuses/destroy/20.json
+// DELETE http://api.twitter.com/1/statuses/destroy/20.json
+twttr.del("/statuses/destroy/20.json");
 
-twttr.post("/statuses/update", {status: "@ev Come here. I need you."});
-	// POST http://api.twitter.com/1/statuses/update.json
-	// status=%40ev%20Come%20here%2e%20I%20need%20you%2e
+// POST http://api.twitter.com/1/statuses/update.json
+// status=%40ev%20Come%20here%2e%20I%20need%20you%2e
+twttr.post("/statuses/update.json", {status: "@ev Come here. I need you."});
--- jquery.rest.js
+++ jquery.rest.js
@@ -3,7 +3,6 @@
 var
 
 	_undefined,
-
 	_slice = Array.prototype.slice,
 	_partial = function (fn) {
 		var args = _slice.call(arguments, 1);
@@ -13,47 +12,38 @@ var
 	},
 
 	verbs = {
-		"GET"    : [ "get"  , "read"   ],
-		"POST"   : [ "post" , "create" ],
-		"PUT"    : [ "put"  , "update" ],
-		"DELETE" : [ "del"  , "delete" ]
+		"GET"    : "get",
+		"POST"   : "post",
+		"PUT"    : "put",
+		"DELETE" : "del"
 	},
 
-	$rest = $.rest = function (url, ext, jsonp) {
+	$rest = $.rest = function (url, options, jsonp) {
 		if (this instanceof $rest) {
-			this.url_   = url;
-			this.ext_   = ext || "";
-			this.jsonp_ = jsonp ? "jsonp" : false;
+			this.url_       = url;
+			this.options_   = options || {};
 		}
 	},
 
 	$restProto = $rest.prototype,
 
-	$ajax = function (type, url, data, success, dataType) {
-		if (typeof data == "function") {
-			dataType = dataType || success;
-			success = data;
-			data = null;
-		}
+	$ajax = function (type, url, options) {
+		
+    options.type = type;
+    options.url = url;
 
-		return $.ajax({
-			url:      url,
-			type:     type,
-			dataType: dataType,
-			success:  success,
-			data:     data
-		});
+		return $.ajax(options);
 	},
 
-	_ajax = function (type, url, data, success, dataType) {
-		return $ajax(type, this.url_ + url, data, success, this.jsonp_ || dataType);
+	_ajax = function (type, url, options) {
+		return $ajax(type, this.url_ + url, options);
 	};
 
 	for (var key in verbs) {
 		if (verbs.hasOwnProperty(key)) {
-			var val = verbs[key], v0 = val[0], v1 = val[1];
-			$rest[v0]      = $rest[v1]      = _partial($ajax, key);
-			$restProto[v0] = $restProto[v1] = _partial(_ajax, key);
+			var val = verbs[key];
+			$rest[val]      = _partial($ajax, key);
+			$restProto[val] = _partial(_ajax, key);
 		}
 	}
 
