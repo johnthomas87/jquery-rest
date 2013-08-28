36d8eee1fdff156c59c1d13f1d93ffb8da4e87f5
Justin <justincyork@gmail.com>
2013-08-28 09:17:21 -0600

    Fix whitespace

--- jquery.rest.js
+++ jquery.rest.js
@@ -1,26 +1,26 @@
 (function ($) {
 
   var verbs = {
-		"GET"    : "get",
-		"POST"   : "post",
-		"PUT"    : "put",
-		"DELETE" : "del"
-	};
+    "GET"    : "get",
+    "POST"   : "post",
+    "PUT"    : "put",
+    "DELETE" : "del"
+  };
 
   // Rest service constructor
-	$.rest = function (url, options, jsonp) {
+  $.rest = function (url, options, jsonp) {
     this.url_       = url;
     this.options_   = options || {};
-	};
+  };
 
-	var $restProto = $.rest.prototype;
+  var $restProto = $.rest.prototype;
   
   $restProto.addOptions = function(options) {
     this.options_ = $.extend(true, this.options_, options);
   };
 
   // Function that wraps $.ajax
-	var _ajax = function (type, url, options) {
+  var _ajax = function (type, url, options) {
     if( typeof options !== 'object' ) {
       options = {};
     }
@@ -38,14 +38,14 @@
     // New options trump; 
     // Maintain both original options by extending an empty object
     options = $.extend(true, {}, this.options_, options);
-		return $.ajax(options);
-	};
+    return $.ajax(options);
+  };
 
   // Add .get, .post, .put, and .del to the rest service prototype
-	$.each(verbs, function(httpMethod, restName){
+  $.each(verbs, function(httpMethod, restName){
     $restProto[restName] = function(url, options){
       return _ajax.call(this, httpMethod, url, options);
     };
-	});
+  });
 
 }(jQuery));
